d78f8b6e2bdb7aa87ed9baccb79a1deceba81a7d
Planter44 <planterplanter777@gmail.com>
2023-06-15 04:20:22 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * Description: main - Prints out a string to stdout.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 if success.
  */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 

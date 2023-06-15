2dae7efa9ff03adeea1ff782f9bbc0a056dd075a
Planter44 <planterplanter777@gmail.com>
2023-06-15 04:31:30 -0700

    Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - Prints out a string to stdout.
- *
  * Return: 1 if success.
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return(1);
 }

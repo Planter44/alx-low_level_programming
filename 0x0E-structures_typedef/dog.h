b86cd42b0c2290fecec220d213e157a486dfb75d
Planter44 <planterplanter777@gmail.com>
2023-07-17 02:52:08 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,9 @@ struct dog
 	char *owner;
 };
 
-// dog_t -typedef for struct the dog
-
+/**
+ * dog_t -typedef for struct the dog.
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);

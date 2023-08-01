43e003b9b397d1fddfc2ee924beb1ed3dfd8b01a
Planter44 <planterplanter777@gmail.com>
2023-07-31 18:39:01 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,8 +5,12 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s -Singly linked list.
- * @n: Integer.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx project
  */
 typedef struct listint_s
 {

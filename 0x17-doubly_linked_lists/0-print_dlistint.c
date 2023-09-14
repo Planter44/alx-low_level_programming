c2ed0363c9ace982d46fa6eb56aaa03a8cb2b67a
Planter44 <planterplanter777@gmail.com>
2023-09-14 10:32:23 -0700

    cohort 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,15 @@
 #include "lists.h"
-
-/*
-* print_dlistint- prints elements in list.
-* @h: points head node.
-* Return: n.
+/**
+* print_dlistint- prints elements of a dlistint_t list.
+* @h: points to head node.
+* Return: nodes.
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t n = 0;
 	const dlistint_t *last = h;
+
 	if (h == NULL)
 	{
 		return (0);
